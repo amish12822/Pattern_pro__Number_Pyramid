5160a2d33dfd834f5f47f3cebfb0c4f0be7492d4
amish12822 <amishofficial23@gmail.com>
2024-07-28 18:43:05 +0530

    add second method to number pyramid

--- number_pyramid.java
+++ number_pyramid.java
@@ -1,29 +1,49 @@
 /**
  * number_pyramid
- */
+*/
 public class number_pyramid {
 
     public static void main(String[] args) {
         
-        int a = 5;
+        // first method
+
+        int a = 9;
         int num = 1;
 
         for ( int i=1; i<=a; i++){
 
             for ( int j=1; j<=a-i; j++){
-
                 System.out.print(" ");
             }
 
-
             for ( int j=1; j<=i; j++){
-
                 System.out.print(num + " ");
-                
             }
 
             num++;
             System.out.println();
         }
+    
+        // Second method
+
+        int n = 9;
+
+        for ( int i=1; i<=n; i++){
+
+            //space
+            int space = ( n-i );
+            for ( int j=1; j<=space; j++){
+                System.out.print(" ");
+            }
+
+            // star 
+            for (int j=1; j<=i; j++){
+                System.out.print(i + " ");
+            }
+
+            System.out.println();
+        }
+
     }
 }
+
